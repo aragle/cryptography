b0946e8a5188d59e959fc5395e9fe68f57433191
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2023-02-17 20:28:10 +0600

    update format

--- one-time-pad/decrypt.c
+++ one-time-pad/decrypt.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void Decrypt(char cipher[], char key[]){
-    int i;
+void Decrypt(char cipher[], char key[])
+{
 
     int len = strlen(cipher);
     char message[1000];
 
-    for(i=0;i<len;i++){
-        message[i] = ((cipher[i]-'A')^(key[i]-'A')) + 'A';
+    for (int i = 0; i < len; i++)
+    {
+        message[i] = ((cipher[i] - 'A') ^ (key[i] - 'A')) + 'A';
     }
     message[len] = '\0';
 
     printf("The decrypted message is: %s\n", message);
 }
 
-int main(){
+int main()
+{
     char cipher[1000], key[1000];
+    
     printf("Enter the encrypted message: ");
     scanf("%s", cipher);
     printf("Enter the key: ");

41a64b471eee18f933bdea879220151f5dbcc594
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2023-02-17 18:27:58 +0600

    add decryption code and update encrytion file

---
+++ one-time-pad/decrypt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+void Decrypt(char cipher[], char key[]){
+    int i;
+
+    int len = strlen(cipher);
+    char message[1000];
+
+    for(i=0;i<len;i++){
+        message[i] = ((cipher[i]-'A')^(key[i]-'A')) + 'A';
+    }
+    message[len] = '\0';
+
+    printf("The decrypted message is: %s\n", message);
+}
+
+int main(){
+    char cipher[1000], key[1000];
+    printf("Enter the encrypted message: ");
+    scanf("%s", cipher);
+    printf("Enter the key: ");
+    scanf("%s", key);
+
+    Decrypt(cipher, key);
+
+    return 0;
+}
--- one-time-pad/encrypt.c
+++ one-time-pad/encrypt.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
 
+void Encrypt(char message[], char key[])
+{
+
+    int len = strlen(message);
+    char cipher[1000];
+
+    for (int i = 0; i < len; i++)
+    {
+        cipher[i] = ((message[i] - 'A') ^ (key[i] - 'A')) + 'A';
+    }
+    cipher[len] = '\0';
+
+    printf("The encrypted message is: %s\n", cipher);
+}
+
+int main()
+{
+    char message[1000], key[1000];
+
+    printf("Enter the message: ");
+    scanf("%s", message);
+    printf("Enter the key: ");
+    scanf("%s", key);
+
+    Encrypt(message, key);
+
+    return 0;
+}
